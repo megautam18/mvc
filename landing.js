520b6411483a9df7cd4ddeb8927272a5ffc4fde3
megautam18 <140488969+megautam18@users.noreply.github.com>
2024-10-19 19:06:47 +0530

    js file for api keys

--- landing.js
+++ landing.js
@@ -6,3 +6,11 @@ function ml(){
 function mr(){
     sb.scrollBy({left:scca, behavior:"smooth"});
 }
+window.onload = function(){
+    fetchmov();
+}
+function fetchmov(){
+    const apikey='76e4ead69f47ca43f4dd5712489b95d4';
+    const apiurl='https://api.themoviedb.org/3/movie/157336?api_key=76e4ead69f47ca43f4dd5712489b95d4';
+    
+}
