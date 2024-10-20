6f01cd3294b4bb7d0e4e998479495ffd80f5d508
megautam18 <140488969+megautam18@users.noreply.github.com>
2024-10-20 20:35:08 +0530

    Search page and API added

--- landing.js
+++ landing.js
@@ -1,5 +1,5 @@
 const sb = document.querySelector('.movcon');
-const scca=300;
+const scca=400;
 function ml(){
     sb.scrollBy({left:-scca, behavior:"smooth"});
 }
@@ -12,5 +12,28 @@ window.onload = function(){
 function fetchmov(){
     const apikey='76e4ead69f47ca43f4dd5712489b95d4';
     const apiurl='https://api.themoviedb.org/3/movie/157336?api_key=76e4ead69f47ca43f4dd5712489b95d4';
-    
+
 }
+
+const searchInput=document.getElementById('serInp');
+const searchButton=document.getElementById('serBtn');
+searchButton.addEventListener('click', async function(){
+    const searchTerm = searchInput.value;
+    const searchUrl='https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1&api_key=76e4ead69f47ca43f4dd5712489b95d4&query='+searchTerm;
+    window.location.href='searchresults.html?query=' + encodeURIComponent(searchTerm);
+    window.onload = function() {
+        resDis(searchUrl);
+    }
+    
+})
+async function resDis(searchUrl){
+    const response = await fetch (searchUrl);
+    const json =  await response.json()
+    const output= json['results'][0];
+    const title = output['original_title'];
+    const rd=output['release_date'];
+    resultsContainer.innerHTML = ''; // Clears everything from the page
+    const movieCard = `<h2 class="title" style="color:white">${title}</h2>
+                        <p class="deets" style="color:white">Release Date: ${rd}</p>`
+    resultsContainer.innerHTML += movieCard;
+    document.body.appendChild(resultsContainer); }
---
+++ searchresults.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const searchTerm = urlParams.get('query');
+    const searchUrl='https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1&api_key=76e4ead69f47ca43f4dd5712489b95d4&query='+searchTerm;
+    resDis(searchUrl);
+}
+async function resDis(seU){
+    const response = await fetch (seU);
+    const json =  await response.json()
+    const output= json['results'][0];
+    const title = output['original_title'];
+    const rd=output['release_date'];
+    const poster='http://image.tmdb.org/t/p/w500'+output['poster_path']
+    resultsContainer.innerHTML = ''; // Clears everything from the page
+    const movieCard = `<h2 class="title" style="color:white">${title}</h2>
+                        <p class="deets" style="color:white">Release Date: ${rd}</p>
+                        <img src="${poster}" width=300 height=400>`
+    resultsContainer.innerHTML += movieCard;
+    document.body.appendChild(resultsContainer); }
